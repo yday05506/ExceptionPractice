b2fd3e7bbb61bc0ee314698d7b1d2c1d66c0ddb4
yday05506 <yangdayeon2@gmail.com>
2023-08-24 09:37:41 +0900

    고전적인 방식으로 예외 처리

--- Program.cs
+++ Program.cs
@@ -14,7 +14,15 @@ namespace ExceptionPractice
             Console.WriteLine("숫자를 입력해주세요 : ");
             int input = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("입력한 위치의 값은 " + array[input] + "입니다.");
+            if(input < array.Length) {
+                Console.WriteLine("입력한 위치의 값은 " + array[input] + "입니다.");
+            }
+            else
+            {
+                Console.WriteLine("인덱스 범위를 넘었습니다.");
+            }
+
+            
         }
     }
 }
