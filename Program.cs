9cb09edc953be5650b8417f88c96957a234e820a
yday05506 <yangdayeon2@gmail.com>
2023-08-24 09:55:30 +0900

    직접 예외 정의 및 발생

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ namespace ExceptionPractice
 {
     internal class Program
     {
+        class WrongNumberException : Exception {
+            public WrongNumberException(string message): base(message)
+            {
+                // 예외가 발생했을 때 특정한 작업을 할 코드를 여기에 적을 수 있음
+            }
+
+        }  
         static void Main(string[] args)
         {
             string[] array = { "가", "나" };
@@ -19,6 +26,12 @@ namespace ExceptionPractice
             try
             {
                 input = int.Parse(Console.ReadLine());
+
+                if(input < 0 || input > 999)
+                {
+                    throw new WrongNumberException("0 이상 999 이하여야 합니다.");
+                }
+
                 Console.WriteLine("입력한 위치의 값은 " + array[input] + "입니다.");
             }
             catch(FormatException ex)
@@ -28,6 +41,11 @@ namespace ExceptionPractice
             catch(IndexOutOfRangeException ex)
             {
                 Console.WriteLine("인덱스 범위를 벗어났습니다.");
+            }
+            catch(WrongNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("제대로 값을 입력하세요.");
             }
             catch(Exception ex) {
                 Console.WriteLine(ex.GetType());
