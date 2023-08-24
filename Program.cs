b9c9e066ee24ea8192b7282fc7b7fa83c7264c9d
yday05506 <yangdayeon2@gmail.com>
2023-08-24 09:17:32 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "가", "나" };
+            Console.WriteLine("숫자를 입력해주세요 : ");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("입력한 위치의 값은 " + array[input] + "입니다.");
+        }
+    }
+}
